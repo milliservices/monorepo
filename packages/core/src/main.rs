faa057c96aa16771c89af8f286f11b3852023292
Akshay Nair <phenax5@gmail.com>
2023-08-28 12:39:04 +0530

    feat: finishes basic service to module communcation

--- packages/core/src/main.rs
+++ packages/core/src/main.rs
@@ -1,36 +1,137 @@
+use anyhow::Result;
+use std::collections::HashMap;
 use wasmtime::*;
 use wasmtime_wasi::{sync::WasiCtxBuilder, WasiCtx};
 
-fn main() -> wasmtime::Result<()> {
-  let engine = Engine::default();
-  let module = Module::from_file(&engine, "./target/wasm32-wasi/debug/example_rust_wasm.wasm")?;
+struct ServiceStore {
+  pub wasi_ctx: WasiCtx,
+  pub metadata: HashMap<String, String>,
+}
 
-  let wasi_ctx = WasiCtxBuilder::new().inherit_stdio().build();
-  let mut store = Store::new(&engine, wasi_ctx);
+struct ServiceModule {
+  // module: Module,
+  engine: Engine,
+  // linker: Linker<ServiceStore>,
+  instance_pre: InstancePre<ServiceStore>,
+}
 
-  let memory_ty = MemoryType::new(1, None);
-  Memory::new(&mut store, memory_ty)?;
+impl ServiceModule {
+  async fn new(path: &str) -> Result<Self> {
+    let mut config = Config::default();
+    config.async_support(true);
+    let engine = Engine::new(&config)?;
+    let module = Module::from_file(&engine, path)?;
 
-  let mut linker = Linker::new(&engine);
-  wasmtime_wasi::add_to_linker(&mut linker, |cx| cx)?;
+    let mut linker: Linker<ServiceStore> = Linker::new(&engine);
+    wasmtime_wasi::add_to_linker(&mut linker, |store: &mut ServiceStore| &mut store.wasi_ctx)?;
 
-  linker.func_wrap(
-    "env",
-    "send_response",
-    |_caller: Caller<'_, WasiCtx>, ptr: i32, size: u32| {
-      println!("Got from WebAssembly {ptr} {size}");
-    },
-  )?;
+    linker.func_wrap(
+      "env",
+      "send_response",
+      |mut caller: Caller<'_, ServiceStore>, ptr: i32, size: u32| -> Result<()> {
+        if let Some(Extern::Memory(memory)) = caller.get_export("memory") {
+          let mut buffer = vec![0u8; size as usize];
+          memory.read(&mut caller, ptr as usize, &mut buffer)?;
+          println!("Got from WebAssembly {:?}", String::from_utf8(buffer));
+        }
+        Ok(())
+      },
+    )?;
 
-  let instance = linker.instantiate(&mut store, &module)?;
-  let memory = instance.get_memory(&mut store, "memory").expect("Nothing");
+    let instance_pre = linker.instantiate_pre(&module)?;
 
-  let pointer: usize = 1;
-  let buffer = &[1, 3, 3, 7];
-  memory.write(&mut store, pointer, buffer)?;
+    Ok(Self {
+      // module,
+      engine,
+      // linker,
+      instance_pre,
+    })
+  }
 
-  let foobar = instance.get_typed_func::<(i32, u32), ()>(&mut store, "on_request")?;
-  foobar.call(&mut store, (pointer as i32, buffer.len() as u32))?;
+  async fn instance(self) -> Result<ServiceInstance> {
+    let inst = ServiceInstance::new(self).await?;
+    Ok(inst)
+  }
+}
+
+pub struct ServiceInstance {
+  instance: Instance,
+  store: Store<ServiceStore>,
+  current_pointer_cursor: u32,
+}
+
+impl ServiceInstance {
+  async fn new(service: ServiceModule) -> Result<Self> {
+    let wasi_ctx = WasiCtxBuilder::new().inherit_stdio().build();
+    let mut store = Store::new(
+      &service.engine,
+      ServiceStore {
+        wasi_ctx,
+        metadata: HashMap::new(),
+      },
+    );
+
+    Memory::new(&mut store, MemoryType::new(1, None))?;
+
+    let instance = service.instance_pre.instantiate_async(&mut store).await?;
+
+    Ok(ServiceInstance {
+      instance,
+      store,
+      current_pointer_cursor: 1,
+    })
+  }
+
+  async fn invoke(&mut self, ptr: i32) -> Result<()> {
+    let on_request = self
+      .instance
+      .get_typed_func::<i32, ()>(&mut self.store, "on_request")?;
+
+    on_request.call_async(&mut self.store, ptr).await?;
+
+    Ok(())
+  }
+
+  fn write_to_memory(&mut self, data: Vec<u8>) -> Result<u32> {
+    let memory = self
+      .instance
+      .get_memory(&mut self.store, "memory")
+      .expect("Nothing");
+
+    let data_ptr = self.current_pointer_cursor;
+    memory.write(&mut self.store, data_ptr as usize, data.as_slice())?;
+
+    // TODO: Cyclic incremenet, maybe?
+    self.current_pointer_cursor += data.len() as u32;
+
+    Ok(data_ptr)
+  }
+
+  fn encode_ptr(&mut self, data: Vec<u8>) -> Result<i32> {
+    let data_len = data.len();
+
+    // Write data to memory
+    let data_ptr = self.write_to_memory(data)?;
+
+    let mut ptr_ptr_buf: Vec<u8> = data_ptr.to_be_bytes().into();
+    let mut ptr_len_buf: Vec<u8> = (data_len as u32).to_be_bytes().into();
+    ptr_ptr_buf.append(&mut ptr_len_buf);
+
+    let ptr = self.write_to_memory(ptr_ptr_buf)?;
+
+    Ok(ptr as i32)
+  }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+  let service_module =
+    ServiceModule::new("./target/wasm32-wasi/debug/example_rust_wasm.wasm").await?;
+
+  let mut instance = service_module.instance().await?;
+
+  let pointer = instance.encode_ptr("Hello world".into())?;
+  instance.invoke(pointer).await?;
 
   Ok(())
 }
--- packages/example-rust-wasm/src/lib.rs
+++ packages/example-rust-wasm/src/lib.rs
@@ -2,14 +2,42 @@ extern "C" {
   fn send_response(input_ptr: i32, input_size: u32);
 }
 
+// fn get_metadata_ptr(key_ptr: i32, key_size: u32) -> i32;
+// fn get_metadata_size(key_ptr: i32, key_size: u32) -> u32;
+//
+// i32->u32
+// fn get_metadata(key: String) -> String;
+
+fn get_mem_representation(ptr: i32) -> (i32, usize) {
+  let mut buf = [0u8; 4];
+  let data_ptr_buf = unsafe { std::slice::from_raw_parts(ptr as *mut u8, 4) };
+  buf.copy_from_slice(data_ptr_buf);
+  let data_ptr = i32::from_be_bytes(buf);
+
+  let data_len_buf = unsafe { std::slice::from_raw_parts((ptr + 4) as *mut u8, 4) };
+  buf.copy_from_slice(data_len_buf);
+  let data_len = u32::from_be_bytes(buf);
+
+  (data_ptr, data_len as usize)
+}
+
+fn get_string_from_ptr(in_ptr: i32) -> String {
+  let (ptr, len) = get_mem_representation(in_ptr);
+  let data_str = unsafe { String::from_raw_parts(ptr as *mut u8, len, len) };
+  let str = data_str.clone();
+  std::mem::forget(data_str);
+
+  str
+}
+
 #[no_mangle]
-extern "C" fn on_request(input_ptr: i32, input_size: u32) {
-  let ptr = input_ptr as *mut u8;
-  let data = unsafe { Vec::from_raw_parts(ptr, input_size as usize, input_size as usize) };
-  println!("{:?}", data);
-  std::mem::forget(data);
+extern "C" fn on_request(input_ptr: i32) {
+  let str = get_string_from_ptr(input_ptr);
+  dbg!(str);
 
   unsafe {
-    send_response(0, 5);
+    let some_buf: Vec<u8> = "FROM WASM".into();
+    let ptr = some_buf.as_ptr();
+    send_response(ptr as i32, some_buf.len() as u32);
   }
 }
