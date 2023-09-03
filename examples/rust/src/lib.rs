97711f3f8f52aa072f6a2dcec3f5c955c8319cc6
Akshay Nair <phenax5@gmail.com>
2023-09-03 18:51:52 +0530

    feat: adds rust utils library

--- examples/rust/src/lib.rs
+++ examples/rust/src/lib.rs
@@ -1,69 +1,13 @@
-use std::mem::size_of;
-
-extern "C" {
-  fn send_response(data_ptr: i32);
-  fn get_metadata(key_ptr: i32) -> i32;
-  fn set_response_metadata(key_ptr: i32, value_ptr: i32);
-  fn call_service(name_ptr: i32, data_ptr: i32) -> i32;
-
-  // fn create_call(name_ptr: i32) -> u32;
-  // fn set_data(id: u32, data_ptr: i32);
-  // fn set_metdata(id: u32, key_ptr: i32, value_ptr: i32);
-  // fn execute_call(id: u32);
-  // fn get_response_metdata(id: u32, data_ptr: i32);
-  // fn get_response_data(id: u32, data_ptr: i32);
-}
-
-fn get_mem_representation(ptr: i32) -> (i32, usize) {
-  let mut buf = [0u8; size_of::<u32>()];
-
-  let data_ptr_buf = unsafe { std::slice::from_raw_parts(ptr as *mut u8, size_of::<i32>()) };
-  buf.copy_from_slice(data_ptr_buf);
-  let data_ptr = i32::from_le_bytes(buf);
-
-  let data_len_buf = unsafe { std::slice::from_raw_parts((ptr + 4) as *mut u8, size_of::<u32>()) };
-  buf.copy_from_slice(data_len_buf);
-  let data_len = u32::from_le_bytes(buf);
-
-  (data_ptr, data_len as usize)
-}
-
-fn read_from_memory(ptr: i32) -> Vec<u8> {
-  let (data_ptr, data_len) = get_mem_representation(ptr);
-  let data_str = unsafe { Vec::from_raw_parts(data_ptr as *mut u8, data_len, data_len) };
-  let str = data_str.clone();
-  std::mem::forget(data_str);
-
-  str
-}
-
-fn write_to_memory(data: Vec<u8>) -> i32 {
-  // let data = data.to_owned();
-  let data_len = data.len();
-  let data_ptr = data.as_ptr() as i32;
-
-  let mut ptr_ptr_buf: Vec<u8> = data_ptr.to_le_bytes().into();
-  let mut ptr_len_buf: Vec<u8> = (data_len as u32).to_le_bytes().into();
-  ptr_ptr_buf.append(&mut ptr_len_buf);
-
-  let ptr_ptr = ptr_ptr_buf.as_ptr() as i32;
-
-  std::mem::forget(data);
-  std::mem::forget(ptr_ptr_buf);
-
-  ptr_ptr
-}
+use milliservices_utils::*;
 
 #[no_mangle]
 extern "C" fn on_request(input_ptr: i32) {
-  let input_buf = read_from_memory(input_ptr);
-  let input_str = String::from_utf8_lossy(input_buf.as_slice());
+  let input_str = read_as_string(input_ptr).expect("aaa cant read input");
   println!(":: [RUST] input = {}", input_str);
 
   unsafe {
     let value_ptr = get_metadata(write_to_memory("X-Authentication".into()));
-    let metadata_value =
-      String::from_utf8(read_from_memory(value_ptr)).expect("Cant read value of key");
+    let metadata_value = read_as_string(value_ptr).expect("Cant read value of key");
     dbg!(metadata_value);
   }
 
@@ -93,22 +37,15 @@ extern "C" fn on_request(input_ptr: i32) {
     println!(":: [RUST] call response 2 = {}", data);
   }
 
-  unsafe {
-    send_response(write_to_memory("Response is coming".into()));
-    send_response(write_to_memory(" again".into()));
-    send_response(write_to_memory(" and again".into()));
-    send_response(write_to_memory(". So much data".into()));
-  };
+  send_string_response("Response is coming".into());
+  send_string_response(" again".into());
+  send_string_response(" and again".into());
+  send_string_response(". So much data".into());
 }
 
 #[no_mangle]
 extern "C" fn final_call(input_ptr: i32) {
-  let input_buf = read_from_memory(input_ptr);
-  let input_str = String::from_utf8_lossy(input_buf.as_slice());
+  let input_str = read_as_string(input_ptr).expect("read input err");
   println!(":: [RUST FINAL] input = {}", input_str);
-
-  unsafe {
-    let output = format!("Final response. With input {input_str}");
-    send_response(write_to_memory(output.into()));
-  };
+  send_string_response(format!("Final response. With input {input_str}"));
 }
