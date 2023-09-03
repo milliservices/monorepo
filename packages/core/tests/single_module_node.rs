61a11f2bc4779cc8f74c7dc3cb825d831706c74d
Akshay Nair <phenax5@gmail.com>
2023-09-03 16:22:02 +0530

    feat: adds test for assemblyscript module-2

--- packages/core/tests/single_module_node.rs
+++ packages/core/tests/single_module_node.rs
@@ -4,7 +4,7 @@ use milliservices_core::node;
 use milliservices_core::service::ModuleConfig;
 
 #[tokio::test]
-async fn single_module_node() {
+async fn simple_node_single_module_1() {
   let node = node::Node::new_ref();
 
   let cfg = ModuleConfig {
@@ -25,4 +25,29 @@ async fn single_module_node() {
   let response = instance.get_response_data().to_owned();
 
   assert_eq!(response, vec![] as Vec<u8>);
+}
+
+#[tokio::test]
+async fn simple_node_single_module_2() {
+  let node = node::Node::new_ref();
+
+  let cfg = ModuleConfig {
+    name: "test-module".to_string(),
+    path: "../../test-fixtures/module-2/lib.wasm".to_string(),
+    symbol: "simple_io".to_string(),
+    ..Default::default()
+  };
+
+  node.lock().await.load_module(cfg).await.expect("module");
+
+  let mut instance = node::spawn_instance(Arc::clone(&node), "test-module".to_string())
+    .await
+    .expect("instance");
+
+  instance.invoke("input data".into()).await.expect("invoke");
+
+  let response_buf = instance.get_response_data().to_owned();
+  let response = String::from_utf8(response_buf);
+
+  assert_eq!(response, Ok("input data. adds output data".to_string()));
 }
--- test-fixtures/module-2/lib.ts
+++ test-fixtures/module-2/lib.ts
@@ -1,1 +1,6 @@
-export function handler_1(input_ptr: i32): void {}
+import { readAsString, sendStringResponse } from '@milliservices/assemblyscript/assembly'
+
+export function simple_io(input_ptr: i32): void {
+  const data = readAsString(input_ptr);
+  sendStringResponse(`${data}. adds output data`)
+}
